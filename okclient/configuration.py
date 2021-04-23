685468901a38d751e0b03b0075983a31e679ac37
generator <generator@mhx.at>
2021-04-23 11:51:43 +0000

    Update to version 0.0.33-frontend-plugins

--- okclient/configuration.py
+++ okclient/configuration.py
@@ -392,7 +392,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 0.11.5".\
+               "SDK Package Version: 0.0.33-frontend-plugins".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
