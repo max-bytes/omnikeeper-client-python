85c0f7bdf2655de7c96641505866dc00d30ad49c
generator <generator@max-bytes.com>
2021-10-09 07:33:14 +0000

    Update to version 2.0.0

--- okclient/__init__.py
+++ okclient/__init__.py
@@ -10,7 +10,7 @@
 """
 
 
-__version__ = "1.8.0"
+__version__ = "2.0.0"
 
 # import ApiClient
 from okclient.api_client import ApiClient
--- okclient/configuration.py
+++ okclient/configuration.py
@@ -392,7 +392,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 1.8.0".\
+               "SDK Package Version: 2.0.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
