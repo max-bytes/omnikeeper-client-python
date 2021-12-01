43b821ef9093e895b33fd9f33456f82e82e05004
generator <generator@max-bytes.com>
2021-12-01 07:57:51 +0000

    Update to version 7.0.0

--- okclient/model/layer_dto.py
+++ okclient/model/layer_dto.py
@@ -24,8 +24,8 @@ from okclient.model_utils import (  # noqa: F401
     file_type,
     none_type,
     validate_get_composed_info,
+    OpenApiModel
 )
-from ..model_utils import OpenApiModel
 from okclient.exceptions import ApiAttributeError
 
 
