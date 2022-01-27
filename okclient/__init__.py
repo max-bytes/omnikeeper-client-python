6bfa73c45c669b713c9e8ab7f14df776078eac8f
generator <generator@max-bytes.com>
2022-01-27 13:40:19 +0000

    Update to version 13.0.0-rc2

--- okclient/__init__.py
+++ okclient/__init__.py
@@ -10,7 +10,7 @@
 """
 
 
-__version__ = "12.0.1"
+__version__ = "13.0.0-rc2"
 
 # import ApiClient
 from okclient.api_client import ApiClient
--- okclient/api_client.py
+++ okclient/api_client.py
@@ -76,7 +76,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/12.0.1/python'
+        self.user_agent = 'OpenAPI-Generator/13.0.0-rc2/python'
 
     def __enter__(self):
         return self
