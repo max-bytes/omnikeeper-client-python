bacd8a91b803224770d3e085a4de169460a79eca
generator <generator@max-bytes.com>
2022-04-04 07:35:40 +0000

    Update to version 15.0.0-rc13

--- okclient/configuration.py
+++ okclient/configuration.py
@@ -395,7 +395,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 15.0.0-rc12".\
+               "SDK Package Version: 15.0.0-rc13".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
