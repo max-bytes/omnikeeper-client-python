5ca95514a35744107ce1b978a4e5fa421cbc4a8a
generator <generator@max-bytes.com>
2022-04-08 13:05:30 +0000

    Update to version 15.0.0-rc26

--- okclient/configuration.py
+++ okclient/configuration.py
@@ -395,7 +395,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 15.0.0-rc25".\
+               "SDK Package Version: 15.0.0-rc26".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
