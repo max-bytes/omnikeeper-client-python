dd27f3999487eafb1052fbdb09ab1b10991055db
generator <generator@max-bytes.com>
2022-04-19 12:38:43 +0000

    Update to version 16.0.0-rc15

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "okclient"
-VERSION = "16.0.0-rc14"
+VERSION = "16.0.0-rc15"
 # To install the library, run the following
 #
 # python setup.py install
