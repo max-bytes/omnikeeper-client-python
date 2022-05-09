d299c7bc4114fcf672833a42bbc657540bfb73e9
generator <generator@max-bytes.com>
2022-05-09 16:10:53 +0000

    Update to version 18.0.0-rc01

--- okclient/configuration.py
+++ okclient/configuration.py
@@ -395,7 +395,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 17.0.0".\
+               "SDK Package Version: 18.0.0-rc01".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
