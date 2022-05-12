d4b89c72d55032eecb4ec18e7f80c39c952195d3
generator <generator@max-bytes.com>
2022-05-12 11:17:31 +0000

    Update to version 18.0.0-rc02

--- okclient/__init__.py
+++ okclient/__init__.py
@@ -10,7 +10,7 @@
 """
 
 
-__version__ = "18.0.0-rc01"
+__version__ = "18.0.0-rc02"
 
 # import ApiClient
 from okclient.api_client import ApiClient
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "okclient"
-VERSION = "18.0.0-rc01"
+VERSION = "18.0.0-rc02"
 # To install the library, run the following
 #
 # python setup.py install
