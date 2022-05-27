b840471c78b8e40a2d5506d6e0934c9c37bafcbe
generator <generator@max-bytes.com>
2022-05-27 20:28:40 +0000

    Update to version 18.0.0-rc26

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "okclient"
-VERSION = "18.0.0-rc25"
+VERSION = "18.0.0-rc26"
 # To install the library, run the following
 #
 # python setup.py install
