c60be58b74468b552e79a67fb7528a01fb641886
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-11-11 16:57:31 +0100

    first functions implemented

--- omnikeeper_client/functions.py
+++ omnikeeper_client/functions.py
@@ -1,29 +1,20 @@
 from oauthlib.oauth2 import LegacyApplicationClient
 from requests_oauthlib import OAuth2Session
-from pythonjsonlogger import jsonlogger
-import logging
-
-def create_logger(level: int):
-    logger = logging.getLogger()
-    logger.setLevel(level)
-
-    # clean up existing stuff
-    list(map(logger.removeHandler, logger.handlers))
-    list(map(logger.removeFilter, logger.filters))
-
-    handler = logging.StreamHandler()
-    handler.setLevel(level)
-
-    formatter = jsonlogger.JsonFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-
-    logger.addHandler(handler)
-
-    return logger
-
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 
 def get_access_token(config: dict) -> str:
     client = LegacyApplicationClient(client_id=config['client_id'])
     oauth = OAuth2Session(client=client)
     token = oauth.fetch_token(token_url=config['token_url'], username=config['username'], password=config['password'])
     return token["access_token"]
+
+def create_graphql_client(url: str, access_token: str) -> Client:
+    transport = AIOHTTPTransport(url=url, headers={'Authorization': "Bearer %s" % access_token})
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+    return client
+
+def execute_graphql(client: Client, query: str, variables: str):
+    prepared_query = gql(query)
+    data = client.execute(query=prepared_query, variables=variables)
+    return data
