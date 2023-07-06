4369421e4f264972aca2170bc3148ca32865af3a
01z <59534221+01z@users.noreply.github.com>
2023-07-06 14:08:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description='Python library containing helper functions for implementing omnikeeper clients',
     author='Maximilian Csuk',
     license='Apache 2.0',
-    install_requires=["webcolors==1.3","aiohttp==3.8.3","gql==3.4.0","requests==2.25.1","numpy==1.21.6","oauthlib==3.2.0","requests-oauthlib==1.3.0","python-json-logger==2.0.2"]
+    install_requires=["webcolors==1.3","aiohttp==3.8.3","gql==3.4.1","requests==2.25.1","oauthlib==3.2.0","requests-oauthlib==1.3.0","python-json-logger==2.0.2"]
 )
