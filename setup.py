d4b3a11a535effa27743d8fd3dfef6b4a2e26d43
Maximilian Csuk <maximilian.csuk@max-bytes.com>
2023-09-14 12:44:09 +0200

    v3.1.1

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 setup(
     name='omnikeeper_client',
     packages=['omnikeeper_client'],
-    version='3.1.0',
+    version='3.1.1',
     description='Python library containing helper functions for implementing omnikeeper clients',
     author='Maximilian Csuk',
     license='Apache 2.0',
-    install_requires=["webcolors==1.3","aiohttp==3.8.3","gql==3.4.1","requests==2.25.1","oauthlib==3.2.0","requests-oauthlib==1.3.0","python-json-logger==2.0.2", "requests_toolbelt==0.10.1"]
+    install_requires=["webcolors==1.3","aiohttp==3.8.3","gql==3.4.1","requests==2.25.1","oauthlib==3.2.0","requests-oauthlib==1.3.0","python-json-logger==2.0.2", "requests_toolbelt==0.10.1", "pandas==2.1.0"]
 )
