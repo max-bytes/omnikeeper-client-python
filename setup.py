8ebbe89cc3c0f0b26faa875b360d11be317fafee
Maximilian Csuk <maximilian.csuk@max-bytes.com>
2023-09-14 11:12:45 +0200

    v3.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='omnikeeper_client',
     packages=['omnikeeper_client'],
-    version='3.0.0',
+    version='3.1.0',
     description='Python library containing helper functions for implementing omnikeeper clients',
     author='Maximilian Csuk',
     license='Apache 2.0',
