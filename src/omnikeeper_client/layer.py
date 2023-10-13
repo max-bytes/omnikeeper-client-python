0c234135ad5c69242b11a6c35f58f33216c01b2e
Markus Hof <markus.hof@mhx.at>
2023-10-13 20:44:24 +0200

    added doc and error handling

--- src/omnikeeper_client/layer.py
+++ src/omnikeeper_client/layer.py
@@ -1,12 +1,36 @@
 import omnikeeper_client as okc
+
 from gql import gql
+from gql.transport.exceptions import (
+    TransportQueryError,
+)
 
 def create_layer(ok_api_client: okc.OkApiClient, layer_id: str) -> bool:
+    """creates a layer by specifying layer_id. 
+
+    Parameters
+    ----------
+    ok_api_client : OkApiClient
+        The OkApiClient instance representing omnikeeper connection
+
+    layer_id : str
+        id of layer to create, must only contain charaters [A-Za-z_]
+
+    Returns
+    -------
+    bool
+        True, if layer was created or was already present. False, if something fails
+    """
+
     query = gql("""
     mutation ($id: String!) {
         manage_createLayer(id: $id) {
             id
         }
     }""")
-    ok_api_client.execute_graphql(query, variables=dict(id=layer_id))
-    return True
+    
+    try:
+        ok_api_client.execute_graphql(query, variables=dict(id=layer_id))
+        return True
+    except TransportQueryError as e:
+        return False
