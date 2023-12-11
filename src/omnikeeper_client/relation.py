df9a1c981fdc6866dc8b7f7e9d03813bdfceb953
Muhamet Kaçuri <MuhametKauri@LAPTOP-CO0D0C5E>
2023-12-11 22:58:05 +0100

    add realtion example for host and interface

--- src/omnikeeper_client/relation.py
+++ src/omnikeeper_client/relation.py
@@ -45,13 +45,7 @@ def get_trait_relation(ok_api_client: okc.OkApiClient, trait_name: str, relation
         dsl_root_type = getattr(ds, f"TERoot_{escaped_trait_name}")
         dsl_wrapper_type = getattr(ds, f"TEWrapper_{escaped_trait_name}")
 
-        result = []
-        for t in dsl_type._type.fields.items():
-            if _is_non_trait_hinted_relation_field(t[1].type):
-                result.append(getattr(dsl_type, t[0]).select(ds.RelatedCIType.relatedCIID))
-
         entity_fields = list(map(lambda t: getattr(dsl_type, t[0]).select(ds.RelatedCIType.relatedCIID), filter(lambda t: t[0] == relation_name and _is_non_trait_hinted_relation_field(t[1].type), dsl_type._type.fields.items())))
-        entity_fields = result
 
         var = DSLVariableDefinitions()
         raw_query = DSLQuery(
@@ -78,8 +72,9 @@ def get_trait_relation(ok_api_client: okc.OkApiClient, trait_name: str, relation
 
         result_list = []
         for entity in result['traitEntities'][prefixed_escaped_trait_name]['all']:
+            # TODO check how related_ciids are mapped from result
             # related_ciids = [related_entity['relatedCIID'] for related_entity in entity]
-            related_ciids = [related_entity['relatedCIID'] for related_entity in entity['entity']['has_host']]
+            related_ciids = [related_entity['relatedCIID'] for related_entity in entity['entity'][relation_name]]
             result_dict = {
                 "base_ciid": entity['ciid'],
                 "related_ciids": related_ciids,
@@ -87,42 +82,3 @@ def get_trait_relation(ok_api_client: okc.OkApiClient, trait_name: str, relation
             result_list.append(result_dict)
 
         return result_list
-    
-def get_traitentities_with_relations_list(ok_api_client: okc.OkApiClient, trait_name: str, relation_name: str, layers: [str]) -> List[Dict[str,Any]]:
-    """
-    Returns all trait entities with relations
-
-    Parameters
-    ----------
-    ok_api_client : OkApiClient
-        The OkApiClient instance representing omnikeeper connection
-
-    trait_name : str
-        the name of the trait to query the data
-
-    layers : [str]
-        list containing layer_ids to search
-
-    Returns
-    -------
-    List[Dict[str,Any]]
-        result all all traitentites with related cis
-    """
-
-    all_traitentities = get_all_traitentities_list(ok_api_client, trait_name=trait_name, layers=layers)
-    all_trait_relations = get_trait_relation(ok_api_client, trait_name=trait_name, relation_name=relation_name, layers=layers)
-
-    # Create a dictionary from all_traitentities for easy lookup
-    dict1 = {d['ciid']: d for d in all_traitentities}
-
-    # Merge the dictionaries
-    traitentities_with_relations = []
-    for d in all_trait_relations:
-        base_ciid = d['base_ciid']
-        related_ciids = d['related_ciids']
-        if base_ciid in dict1:
-            merged_dict = dict1[base_ciid].copy()  # copy the dictionary to not modify the original
-            merged_dict['related_ciis'] = [dict1[ciid] for ciid in related_ciids if ciid in dict1]
-            traitentities_with_relations.append(merged_dict)
-
-    return traitentities_with_relations
